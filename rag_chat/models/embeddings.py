2595d6f35363ed2fad860c03aebfb281a236cafa
Aesha <swatidutta993@gmail.com>
2025-04-25 07:54:53 +0545

    Change embedding model according to the added reranking

--- rag_chat/models/embeddings.py
+++ rag_chat/models/embeddings.py
@@ -1,18 +1,16 @@
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
-import logging
-from rag_chat.configs.logging_config import setup_logging
+import numpy as np
 
 class EmbeddingModel:
-    def __init__(self, model_name='sentence-transformers/all-MiniLM-L6-v2'):
-        self.embedding = HuggingFaceEmbeddings(model_name=model_name)
+    def __init__(self):
+        self.model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-    def embed_docs(self, docs):
-        return Chroma.from_documents(documents=docs, embedding=self.embedding)
+    def embed_text(self, text: str):
+        # Embed a single text query
+        return self.model.embed_query(text)
 
-    def verify_vectors(self, docs):
-        logging.info("Verifying vector embedding")
-        v1 = self.embedding.embed_query(docs[0].page_content)
-        v2 = self.embedding.embed_query(docs[1].page_content)
+    def embed_texts(self, docs):
+        # Extract plain text from Document objects and embed them
+        texts = [doc.page_content for doc in docs]  # Extract text content from Document
+        return self.model.embed_documents(texts)  # Return document embeddings
 
-        logging.info(f"Vector 1: {v1[:5]}")
